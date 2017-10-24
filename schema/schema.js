82a3fa6d66778318f02b9f9b4131ad683af205c5
Renan Martins <martins@fujisan.co.jp>
2017-10-24 10:16:57 -0700

    Add editUser mutation

--- schema/schema.js
+++ schema/schema.js
@@ -113,6 +113,27 @@ const mutation = new GraphQLObjectType({
         return axios.delete(`${API_URL}/users/${args.id}`)
           .then(response => response.data);
       }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        firstName: {
+          type: GraphQLString,
+        },
+        age: {
+          type: GraphQLInt,
+        },
+        companyId: {
+          type: GraphQLString,
+        }
+      },
+      resolve(parentValue, args) {
+        return axios.patch(`${API_URL}/users/${args.id}`, args)
+        .then(response => response.data);
+      },
     }
   }
 });
